0bbfd7a86d21f96355316a6ff09c97bec08c71b1
Matt Slawson <mdslawson@gmail.com>
2015-04-04 12:12:39 -0500

    Added bootstrap ui and began work on collapsing columns.  Having some CSS conflicts that need cleaned up.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -18,6 +18,29 @@ app.controller('MainCtrl', ['$scope','productsBase', function ($scope, productsB
 
   	// // var products = $firebaseArray(productsRef);
 
+// Logic for dropdown menu
+  $scope.oneAtATime = true;
+
+  $scope.groups = [
+    {
+      title: 'Dynamic Group Header - 1',
+      content: 'Dynamic Group Body - 1'
+    },
+    {
+      title: 'Dynamic Group Header - 2',
+      content: 'Dynamic Group Body - 2'
+    }
+  ];
+
+  $scope.items = ['Item 1', 'Item 2', 'Item 3'];
+
+  $scope.addItem = function() {
+    var newItemNo = $scope.items.length + 1;
+    $scope.items.push('Item ' + newItemNo);
+  };
+
+// logic for products
+
   	$scope.products = productsBase;
 
   	$scope.addProduct = function(){
