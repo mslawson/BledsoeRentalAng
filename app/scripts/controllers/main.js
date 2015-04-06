7f1a58cc6a50fe1b7d133e570adb122ad5f0c8a7
Matt Slawson <mdslawson@gmail.com>
2015-04-06 14:47:18 -0500

    Updated and completed working categories that are fed from groups in main.js

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -23,7 +23,7 @@ app.controller('MainCtrl', ['$scope','productsBase', function ($scope, productsB
 
   $scope.groups = [
     {
-      title: 'Concrete & Masonry',
+      title: 'Concrete',
       content: [
         {id:1, name:'Grinders'},
         {id:2, name:'Polishers'},
@@ -33,16 +33,94 @@ app.controller('MainCtrl', ['$scope','productsBase', function ($scope, productsB
     {
       title: 'Drills & Hammers',
       content: [
-        {id:1, name:'Dynamic Group Body - 1'}
+        {id:1, name:'Drill item #1'},
+        {id:2, name:'Drill item #2'},
+        {id:3, name:'Drill item #3'}
       ]
     },
     {
       title: 'Floor Mainentance',
       content: [
-        {id:1, name:'Dynamic Group Body - 1'}
+        {id:1, name:'Floor item #1'}
+      ]
+    },
+    {
+      title: 'Generators & Pumps',
+      content: [
+        {id:1, name:'Generator item #1'}
+      ]
+    },
+    {
+      title: 'Heating, Cooling & Steamers',
+      content: [
+        {id:1, name:'Heating item #1'}
+      ]
+    },
+    {
+      title: 'Heavy Equipment',
+      content: [
+        {id:1, name:'Equipment item #1'}
+      ]
+    },
+    {
+      title: 'Jacks & Hoisting Equipment',
+      content: [
+        {id:1, name:'Hoisting item #1'}
+      ]
+    },
+    {
+      title: 'Ladders & Scaffolding',
+      content: [
+        {id:1, name:'Ladders item #1'}
+      ]
+    },
+    {
+      title: 'Lawn Equipment',
+      content: [
+        {id:1, name:'Lawn item #1'}
+      ]
+    },
+    {
+      title: 'Moving & Trucks',
+      content: [
+        {id:1, name:'Moving item #1'}
+      ]
+    },
+    {
+      title: 'Party Rentals',
+      content: [
+        {id:1, name:'Party item #1'}
+      ]
+    },
+    {
+      title: 'Paints & Spray',
+      content: [
+        {id:1, name:'Paints item #1'}
+      ]
+    },
+    {
+      title: 'Plumbing & Sewer',
+      content: [
+        {id:1, name:'Plumbing item #1'}
+      ]
+    },
+    {
+      title: 'Saws',
+      content: [
+        {id:1, name:'Saws item #1'}
       ]
     }
   ];
+
+  $scope.currentCategory = "";
+
+  $scope.foo = function(title){
+    alert(title);
+  };
+
+  $scope.setCurrentCategory = function(category) {
+    $scope.currentCategory = category;
+  };
 
   $scope.items = ['Item 1', 'Item 2', 'Item 3'];
 
