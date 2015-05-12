c1cfb0a8fac31090245a0c34b5a393866d33b2ac
Matt Slawson <mdslawson@gmail.com>
2015-05-12 16:34:59 -0500

    Finished popout layouts.  Need to create additional layouts.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,12 +11,103 @@
 
  var app = angular.module('angfireApp');
 
+ app.controller('firestationModal', function ($scope, $modal, $log) {
+
+  // $scope.items = ['Cost:', 'Size:', 'Occupancy:'];
+  $scope.animationsEnabled = true;
+  $scope.open = function (size) {
+
+    var modalInstance = $modal.open({
+      animation: $scope.animationsEnabled,
+      templateUrl: 'firestationModalContent.html',
+      controller: 'ModalInstanceCtrl',
+      size: size,
+      resolve: {
+        items: function () {
+          return $scope.items;
+        }
+      }
+    });
+
+    modalInstance.result.then(function (selectedItem) {
+      $scope.selected = selectedItem;
+    }, function () {
+      $log.info('Modal dismissed at: ' + new Date());
+    });
+  };
+
+  $scope.toggleAnimation = function () {
+    $scope.animationsEnabled = !$scope.animationsEnabled;
+  };
+
+});
+
+ app.controller('princessModal', function ($scope, $modal, $log) {
+
+  // $scope.items = ['Cost:', 'Size:', 'Occupancy:'];
+  $scope.animationsEnabled = true;
+  $scope.open = function (size) {
+
+    var modalInstance = $modal.open({
+      animation: $scope.animationsEnabled,
+      templateUrl: 'princessModalContent.html',
+      controller: 'ModalInstanceCtrl',
+      size: size,
+      resolve: {
+        items: function () {
+          return $scope.items;
+        }
+      }
+    });
+
+    modalInstance.result.then(function (selectedItem) {
+      $scope.selected = selectedItem;
+    }, function () {
+      $log.info('Modal dismissed at: ' + new Date());
+    });
+  };
+
+  $scope.toggleAnimation = function () {
+    $scope.animationsEnabled = !$scope.animationsEnabled;
+  };
+
+});
+
+// Please note that $modalInstance represents a modal window (instance) dependency.
+// It is not the same as the $modal service used above.
+
+app.controller('ModalInstanceCtrl', function ($scope, $modalInstance, items) {
+
+  $scope.items = items;
+  $scope.selected = {
+    item: $scope.items[0]
+  };
+
+  $scope.ok = function () {
+    $modalInstance.close($scope.selected.item);
+  };
+
+  $scope.cancel = function () {
+    $modalInstance.dismiss('cancel');
+  };
+});
+
+
  app.controller('MainCtrl', function ($scope, $location, productsBase, servCategory, $anchorScroll ) {
     // var productsRef = new Firebase('https://bledsoe.firebaseio.com/');
 
     // var products = $firebaseArray(productsRef);
 
     // // var products = $firebaseArray(productsRef);
+
+
+
+
+
+
+
+
+
 
 
     $scope.gotoTop = function() {
@@ -60,10 +151,8 @@
 
   {
     title: 'Safety',
-    intro: "Even though we pride ourselves on having the most extravagant inflatable bounce houses in the area, safety is still our number one priority. When having any special event or party, the last thing you want to happen is have someone get hurt. <br><br> Every material used in our inflatables was engineered with safety and quality in mind. The materials used to create our inflatables are both lead-safe and flame-retardant. Many of the vinyl's used to create other manufactures bounces houses are made with toxic chemicals and are not resistant to fire.<br><br> Our inflatables also include features such as Finger-Safe Netting and sun shades. These features help to prevent children from flipping and causing injury. This is a must-have in any inflatable. <br><br> One of the most common concerns with inflatables is that the blower used to keep the bounce house inflated will stop working. This can happen due to a power outage, the blower becoming unplugged, or kids tampering with the blower and shutting it off. The last thing you want to happen is for the inflatable to collapse on the participants inside without anyone being alerted. That is why we send out all of our inflatable rentals with the WATCHDOG Blower-Siren free of charge to the customer. In the event that the blower should stop working in ANY event, the WATCHDOG Blower-Siren will sound a 120-decible siren that will let all participants in and outside of the inflatable know that the blower is not operating which should allow participants enough to time to exit the inflatable without harm.",
-    text1: "Every material used in our inflatables was engineered with safety and quality in mind. The materials used to create our inflatables are both lead-safe and flame-retardant. Many of the vinyl's used to create other manufactures bounces houses are made with toxic chemicals and are not resistant to fire.",
-    text2: "Our inflatables also include features such as Finger-Safe Netting and sun shades. These features help to prevent children from flipping and causing injury. This is a must-have in any inflatable.",
-    text3: "One of the most common concerns with inflatables is that the blower used to keep the bounce house inflated will stop working. This can happen due to a power outage, the blower becoming unplugged, or kids tampering with the blower and shutting it off. The last thing you want to happen is for the inflatable to collapse on the participants inside without anyone being alerted. That is why we send out all of our inflatable rentals with the WATCHDOG Blower-Siren free of charge to the customer. In the event that the blower should stop working in ANY event, the WATCHDOG Blower-Siren will sound a 120-decible siren that will let all participants in and outside of the inflatable know that the blower is not operating which should allow participants enough to time to exit the inflatable without harm.",
+    intro: "Even though we pride ourselves on having the most extravagant inflatable bounce houses in the area, safety is still our number one priority. When having any special event or party, the last thing you want to happen is have someone get hurt. <br><br> Every material used in our inflatables was engineered with safety and quality in mind. The materials used to create our inflatables are both lead-safe and flame-retardant. Many of the vinyl's used to create other manufactures bounces houses are made with toxic chemicals and are not resistant to fire.<br><br> Our inflatables also include features such as Finger-Safe Netting and sun shades. These features help to prevent children from flipping and causing injury. This is a must-have in any inflatable. <br><br>  One of the most common concerns with inflatables is that the blower used to keep the bounce house inflated will stop working. This can happen due to a power outage, the blower becoming unplugged, or kids tampering with the blower and shutting it off. The last thing you want to happen is for the inflatable to collapse on the participants inside without anyone being alerted. That is why we send out all of our inflatable rentals with the WATCHDOG Blower-Siren free of charge to the customer. In the event that the blower should stop working in ANY event, the WATCHDOG Blower-Siren will sound a 120-decible siren that will let all participants in and outside of the inflatable know that the blower is not operating which should allow participants enough to time to exit the inflatable without harm.",
+    
     pic1: 'images/bounce/wd.jpg',
     picalign: '"alignleft"'
   },
