b2f5fb585f53d7dd284acc40968505c7f2ba576a
Matt Slawson <mdslawson@gmail.com>
2015-05-19 11:30:16 -0500

    Added wackycastle to bouncers

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,6 +11,37 @@
 
  var app = angular.module('angfireApp');
 
+  app.controller('wackycastleModal', function ($scope, $modal, $log) {
+
+  // $scope.items = ['Cost:', 'Size:', 'Occupancy:'];
+  $scope.animationsEnabled = true;
+  $scope.open = function (size) {
+
+    var modalInstance = $modal.open({
+      animation: $scope.animationsEnabled,
+      templateUrl: 'wackycastleModalContent.html',
+      controller: 'ModalInstanceCtrl',
+      size: size,
+      resolve: {
+        items: function () {
+          return $scope.items;
+        }
+      }
+    });
+
+    modalInstance.result.then(function (selectedItem) {
+      $scope.selected = selectedItem;
+    }, function () {
+      $log.info('Modal dismissed at: ' + new Date());
+    });
+  };
+
+  $scope.toggleAnimation = function () {
+    $scope.animationsEnabled = !$scope.animationsEnabled;
+  };
+
+});
+
  app.controller('tigerbellyModal', function ($scope, $modal, $log) {
 
   // $scope.items = ['Cost:', 'Size:', 'Occupancy:'];
